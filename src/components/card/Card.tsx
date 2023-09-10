8aeb968224055713cc7fec5c5ccc5c25eb8d639e
YazykovaDaria <yazykovadasva@gmail.com>
2023-09-10 21:15:33 +0300

    feat: card

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import GoogleBook from 'src/types/book';
+import { Card, Box, CardContent, CardMedia, Typography } from '@mui/material';
+
+type Props = { book: GoogleBook };
+
+function BookCard({ book }: Props) {
+  const handleClick = () => {
+    console.log(book);
+  };
+
+  const info = book.volumeInfo;
+
+  return (
+    <div onClick={handleClick}>
+      <Card
+        sx={{
+          maxWidth: 300,
+          '&:hover': {
+            transform: 'scale(1.1)',
+            cursor: 'pointer',
+            zIndex: '10',
+          },
+        }}
+      >
+        <CardMedia
+          component="img"
+          alt=" "
+          height="140"
+          image={info.imageLinks?.smallThumbnail || ''}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {info.title}
+          </Typography>
+
+          <Box>
+            <Typography variant="body2" color="#0d47a1">
+              Authors
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {info.authors?.length && info.authors.join(', ')}
+            </Typography>
+          </Box>
+
+          <Box>
+            <Typography variant="body2" color="#0d47a1">
+              Category
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {info.categories?.length && info.categories[0]}
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default BookCard;
