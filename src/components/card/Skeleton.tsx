ef47184df88679f9d82858890235729fd6805a4e
YazykovaDaria <yazykovadasva@gmail.com>
2023-09-10 21:14:17 +0300

    feat: skeleton

---
+++ src/components/card/Skeleton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { CircularProgress, Box } from '@mui/material';
+
+function Skeleton() {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        background: '#d8d7d7',
+        zIndex: '10',
+        opacity: '0.8',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        top: '0',
+        left: '0',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <CircularProgress></CircularProgress>
+    </Box>
+  );
+}
+
+export default Skeleton;
