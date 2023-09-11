43faa9fa7b0ffbf46f046d6b43e75c6752b60808
YazykovaDaria <yazykovadasva@gmail.com>
2023-09-11 13:14:41 +0300

    refactor: props + select

--- src/components/searchSection/SearchSection.tsx
+++ src/components/searchSection/SearchSection.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
-import SearchForm from './SearchForm';
 import { useAppSelector, useAppDispatch } from 'src/hooks/reduxHook';
-import { setSorting, setFiltering } from 'src/redux/slices/booksQueryData';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Typography, MenuItem, Box, Grid } from '@mui/material';
+import { setSorting, setFiltering, setSearch } from 'src/redux/slices/booksQueryData';
 
-function SearchSection() {
+import { SelectChangeEvent } from '@mui/material/Select';
+import { Typography, Box, Grid } from '@mui/material';
+
+import SearchForm from './SearchForm';
+import SelectInput from './SelectInput';
+
+type Props = {
+  startSearch: () => void;
+};
+
+function SearchSection({ startSearch }: Props) {
   const dispatch = useAppDispatch();
-  const { orderBy, filtering } = useAppSelector((state) => state.booksQueryData);
+  const { orderBy, filtering, search } = useAppSelector((state) => state.booksQueryData);
+
+  const handleSearch = (data: string) => {
+    dispatch(setSearch(data));
+    startSearch();
+  };
+
+  const searchArguments = {
+    label: 'Enter a search term',
+    onSubmit: handleSearch,
+    inputValue: search,
+  };
 
   const hangleSorting = (event: SelectChangeEvent) => {
     dispatch(setSorting(event.target.value));
   };
 
+  const sortintArguments = {
+    inputValue: orderBy,
+    title: 'Sorting by',
+    onChange: hangleSorting,
+    values: ['relevance', 'newest'],
+  };
+
   const handleFiltering = (event: SelectChangeEvent) => {
     dispatch(setFiltering(event.target.value));
+    startSearch();
+  };
+
+  const filteringArguments = {
+    inputValue: filtering,
+    title: 'Categories',
+    onChange: handleFiltering,
+    values: ['all', 'art', 'biography', 'computers', 'history', 'medical', 'poetry'],
   };
 
   return (
@@ -30,44 +63,17 @@ function SearchSection() {
         background: 'RGBA(255, 255, 204, 0.8)',
       }}
     >
-      <Typography variant="h3">Search for books</Typography>
-      <SearchForm></SearchForm>
+      <Typography variant="h3" align="center">
+        Search for books
+      </Typography>
+      <SearchForm {...searchArguments}></SearchForm>
 
       <Grid container spacing={2} justifyContent="center" alignItems="center">
         <Grid item>
-          <div>
-            <Typography>Sorting by</Typography>
-            <Select
-              sx={{ width: '200px' }}
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={orderBy}
-              onChange={hangleSorting}
-            >
-              <MenuItem value={'relevance'}>relevance</MenuItem>
-              <MenuItem value={'newest'}>newest</MenuItem>
-            </Select>
-          </div>
+          <SelectInput {...sortintArguments}></SelectInput>
         </Grid>
         <Grid item>
-          <div>
-            <Typography>Categories</Typography>
-            <Select
-              labelId="categories"
-              id="categories"
-              value={filtering}
-              onChange={handleFiltering}
-              sx={{ width: '200px' }}
-            >
-              <MenuItem value={'all'}>all</MenuItem>
-              <MenuItem value={'art'}>art</MenuItem>
-              <MenuItem value={'biography'}>biography</MenuItem>
-              <MenuItem value={'computers'}>computers</MenuItem>
-              <MenuItem value={'history'}>history</MenuItem>
-              <MenuItem value={'medical'}>medical</MenuItem>
-              <MenuItem value={'poetry'}>poetry</MenuItem>
-            </Select>
-          </div>
+          <SelectInput {...filteringArguments}></SelectInput>
         </Grid>
       </Grid>
     </Box>
---
+++ src/components/searchSection/SelectInput.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Typography, MenuItem } from '@mui/material';
+
+type Props = {
+  inputValue: string;
+  title: string;
+  onChange: (event: SelectChangeEvent) => void;
+  values: string[];
+};
+
+function SelectInput({ inputValue, title, onChange, values }: Props) {
+  return (
+    <div>
+      <Typography>{title}</Typography>
+      <Select
+        sx={{ width: '200px' }}
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={inputValue}
+        onChange={onChange}
+      >
+        {values.map((value) => (
+          <MenuItem value={value} key={value}>
+            {value}
+          </MenuItem>
+        ))}
+      </Select>
+    </div>
+  );
+}
+
+export default SelectInput;
